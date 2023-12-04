2a9390b8fddb25a3f662bccbdf559667537354a4
Carlos Solis <mrsoliscalderon@gmail.com>
2023-12-04 18:51:33 +0100

    add connecto to db

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import createDebug from 'debug';
 
+const debug = createDebug('W8E:app');
+
+export const app = express();
+debug('Starting');
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.static('public'));
---
+++ src/services/db.connect.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+export const dbConnect = () => {
+  const user = process.env.USER_DB;
+  const passwd = process.env.PASSWD_DB;
+  const cluster = process.env.DB_CLUSTER;
+  const dataBase = process.env.DB_NAME;
+  const uri = `mongodb+srv://${user}:${passwd}@${cluster}/${dataBase}/?retryWrites=true&w=majority`;
+  return mongoose.connect(uri);
+};
+// mongodb+srv://mrsoliscalderon:<password>@cluster0.sjl4iz6.mongodb.net/?retryWrites=true&w=majority
