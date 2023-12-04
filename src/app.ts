ede793b4812676bd3f60dee22fdb5b0f005a049a
Carlos Solis <mrsoliscalderon@gmail.com>
2023-12-04 21:24:25 +0100

    create entities, schema and types

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+// Import { monumentsRouter } from './routers/monuments.routes.js';
 import createDebug from 'debug';
+import { errorMiddleware } from './middleware/error.middleware.js';
+// Import { usersRouter } from './routers/users.routes.js';
 
-const debug = createDebug('W8E:app');
+const debug = createDebug('EPV:app');
 
 export const app = express();
 debug('Starting');
@@ -13,3 +16,8 @@ app.use(morgan('dev'));
 
 app.use(express.json());
 app.use(express.static('public'));
+
+// App.use('/monuments', monumentsRouter);
+// app.use('/users', usersRouter);
+
+app.use(errorMiddleware);
---
+++ src/entities/monument.model.ts
@@ -0,0 +1,11 @@
+import { ImgData } from '../types/img.data.js';
+import { User } from './user.js';
+
+export type Monument = {
+  id: string;
+  name: string;
+  culture: string;
+  description: string;
+  author: User;
+  monumentImg: ImgData;
+};
