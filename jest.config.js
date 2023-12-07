d08d1d49c69c1e1c5d01c21ab2f7eb76710a68b2
Carlos Solis <mrsoliscalderon@gmail.com>
2023-12-07 12:00:59 +0100

    add test to middlewares

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,7 @@ export default {
   testPathIgnorePatterns: ['dist'],
   resolver: 'jest-ts-webcompat-resolver',
   coveragePathIgnorePatterns: [
-    'src/controllers/controller.ts',
     'src/repos/users/users.mongo.model.ts',
-    'src/repos/repo.ts',
     'src/app.ts',
     'src/index.ts',
     'src/routers/users.router.ts',
