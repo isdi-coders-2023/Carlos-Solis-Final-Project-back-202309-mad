a4d2cddc04b28c096f235c86426c711678b87337
Carlos Solis <mrsoliscalderon@gmail.com>
2023-12-11 09:54:37 +0100

    add file interceptor and test

--- src/middleware/file.interceptor.test.ts
+++ src/middleware/file.interceptor.test.ts
@@ -3,23 +3,23 @@ import { FileInterceptor } from './file.interceptor.js';
 import multer from 'multer';
 
 jest.mock('multer');
-
-describe('Given FileInterceptor', () => {
-  const middlewareMock = jest.fn();
-  const single = jest.fn().mockReturnValue(middlewareMock);
+describe('Given...', () => {
+  // Given
+  const midlewareMock = jest.fn();
+  const single = jest.fn().mockReturnValue(midlewareMock);
   multer.diskStorage = jest
     .fn()
     .mockImplementation(({ filename }) => filename('', '', () => {}));
   (multer as unknown as jest.Mock).mockReturnValue({ single });
 
-  describe('When we instantiate it', () => {
+  describe('When we instantiate', () => {
     const interceptor = new FileInterceptor();
 
-    test('Then singleFileStore should be used', () => {
+    test('Then it should be...', () => {
       interceptor.singleFileStore()({} as Request, {} as Response, jest.fn());
       expect(multer.diskStorage).toHaveBeenCalled();
       expect(single).toHaveBeenCalled();
-      expect(middlewareMock).toHaveBeenCalled();
+      expect(midlewareMock).toHaveBeenCalled();
     });
   });
 });
